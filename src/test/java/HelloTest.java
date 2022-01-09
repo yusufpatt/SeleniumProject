7eb8422db22e3e46562564bf4b0a7e9b3f862069
yusufpatt <yusufpat28@gmail.com>
2022-01-10 01:22:51 +0300

    Added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,6 +3,7 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("Hello Test");
+        System.out.println("About to share project on GitHub");
 
     }
 }
